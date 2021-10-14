c15c261ec9252d794927e2dbd5732eea18c78c00
fight1991 <359193813@qq.com>
2021-10-14 13:05:11 +0800

    首页初步完成

--- lib/src/pages/home/home_main_page.dart
+++ lib/src/pages/home/home_main_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_car_live/utils/log_utils.dart';
+import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -23,9 +25,39 @@ class _MainPage extends State<MainPage> {
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
         child: Column(
-          children: [buildGridBox(), buildBtnBox()],
+          children: [
+            buildCouponBox(), //优惠券轮播区域
+            buildGridBox(), //业务按钮区域
+            buildBtnBox(), //电子车牌申领区域
+          ],
         ),
       ),
+    );
+  }
+
+  Widget buildCouponBox() {
+    print(MediaQuery.of(context).size);
+    return Container(
+      margin: EdgeInsets.only(bottom: 15),
+      height: MediaQuery.of(context).size.width * (178 / 690),
+      // width: MediaQuery.of(context).size.width,
+      child: Swiper(
+          itemCount: 3,
+          // viewportFraction: 0.8,
+          // scale: 0.9,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              margin: EdgeInsets.symmetric(horizontal: 5),
+              child: Image.asset(
+                'assets/images/banner1.png',
+                fit: BoxFit.fill,
+              ),
+            );
+          },
+          pagination: SwiperPagination(alignment: Alignment.bottomCenter),
+          onTap: (int index) {
+            LogUtils.e('点击轮播图$index');
+          }),
     );
   }
 
