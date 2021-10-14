f9534b0e32c3f3d156f99fa716bfef10891c87f0
fight1991 <359193813@qq.com>
2021-10-14 16:48:47 +0800

    暂存

--- lib/src/pages/home/home_main_page.dart
+++ lib/src/pages/home/home_main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_car_live/utils/log_utils.dart';
+import 'package:flutter_car_live/widgets/iconfont/iconfont.dart';
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 
 class MainPage extends StatefulWidget {
@@ -16,7 +17,7 @@ class _MainPage extends State<MainPage> {
         title: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Icon(Icons.location_on),
+            Icon(IconFont.icon_position),
             SizedBox(width: 5),
             Text('恒大财富中心'),
           ],
