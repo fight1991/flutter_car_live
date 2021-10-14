b3f957d51945e7a0830ea72483b03240f4e7bb3f
fight1991 <359193813@qq.com>
2021-10-14 09:11:08 +0800

    暂存

--- lib/src/pages/home/home_main_page.dart
+++ lib/src/pages/home/home_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_car_live/src/pages/components/TopBg.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -10,19 +9,17 @@ class MainPage extends StatefulWidget {
 class _MainPage extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TopBg(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(Icons.location_on),
-              SizedBox(width: 5),
-              Text('恒大财富中心'),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.location_on),
+            SizedBox(width: 5),
+            Text('恒大财富中心'),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
