f5e06f558c68af27c1ee56e650e34acc9c7c96aa
fight1991 <359193813@qq.com>
2021-10-14 09:38:36 +0800

    添加图片

--- lib/src/pages/home/home_main_page.dart
+++ lib/src/pages/home/home_main_page.dart
@@ -20,6 +20,44 @@ class _MainPage extends State<MainPage> {
           ],
         ),
       ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        child: Column(
+          children: [buildGridBox()],
+        ),
+      ),
+    );
+  }
+
+  Widget buildGridBox() {
+    return Container(
+      padding: EdgeInsets.all(18),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6), color: Colors.white),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          girdBtnItem(iconName: 'oil.png', label: '无感加油'),
+          girdBtnItem(iconName: 'maintenance.png', label: '维修保养'),
+          girdBtnItem(iconName: 'wash.png', label: '洗车美容'),
+          girdBtnItem(iconName: 'parking.png', label: '智慧停车'),
+        ],
+      ),
+    );
+  }
+
+  Widget girdBtnItem({String iconName = 'oil.png', String label = ''}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset(
+          'assets/images/$iconName',
+          width: 64,
+          height: 64,
+        ),
+        SizedBox(height: 5),
+        Text(label)
+      ],
     );
   }
 }
